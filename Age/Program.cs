d1097ad0f7eee96e9fb9bcbafaa6884d0dd68163
AdelyaGI <112945520+adeelgilfanova@users.noreply.github.com>
2022-09-30 12:45:28 +0300

    all

---
+++ Age/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+
+    class Program
+    {
+        private static int CalculateAge(DateTime birthDate)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthDate.Year;
+            return today.Year - birthDate.Year - 1 +((today.Month > birthDate.Month || today.Month == birthDate.Month && today.Day >= birthDate.Day) ? 1 : 0);
+        }
+        static void Main(string[] args)
+        {
+            DateTime birthDate = new DateTime(2004, 5, 26);
+            int age = CalculateAge(birthDate);
+            Console.WriteLine("{0}", age);
+        }
+    }
+}
